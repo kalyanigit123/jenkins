e9f29fe1dbbf3de8521657c1a48381af8e47a9b4
kalyanigit123 <160001839+kalyanigit123@users.noreply.github.com>
2025-02-03 11:25:49 +0530

    Create jenkinsfile-docker.groovy

---
+++ jenkinsfile-docker.groovy
@@ -0,0 +1,30 @@
+pipeline{
+    agent any
+    stages{
+      stage('Build'){
+        steps{
+             sh ''' cd $WORKSPACE
+             docker build -t spring:v${BUILD_NUMBER} .'''
+      }
+}
+stage('docker upload'){
+ steps{
+    sh """
+      docker tag spring:v${BUILD_NUMBER} ekalyani/reactjs:spring-v${BUILD_NUMBER}
+      docker push ekalyani/reactjs:spring-v${BUILD_NUMBER}
+      """
+ }
+}
+stage(push ECR){
+    steps{
+        sh '''
+        aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 545684843058.dkr.ecr.ap-south-1.amazonaws.com
+
+        docker tag simple-web-app:v${BUILD_NUMBER} 545684843058.dkr.ecr.ap-south-1.amazonaws.com/simple-web-app:V${BUILD_NUMBER}
+        docker push 545684843058.dkr.ecr.ap-south-1.amazonaws.com/simple-web-app:v${BUILD_NUMBER}
+        '''
+    }
+}
+}
+
+ }
